4fe1cd1c41af587eb2b94aba244cf4e84ce281b1
ICECREAMcode-cell <carlitosfloresalvaro0412@gmail.com>
2026-03-04 18:45:37 -0400

    archivos data,controllers,models y views

---
+++ NovahApp/Data/DbContext.cs
@@ -0,0 +1,35 @@
+using Microsoft.Data.SqlClient;
+
+namespace TiendaApp.Data
+{
+    public class DbContext
+    {
+        private static DbContext? _instance;
+        private static readonly object _lock = new object();
+        private readonly string _connStr = "Server=DESKTOP-PMI0ES2\\SQLEXPRESS;Database=Nova_Db;Trusted_Connection=True;TrustServerCertificate=True";
+
+        private DbContext() { }
+
+
+        public static DbContext Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                            _instance = new DbContext();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connStr);
+        }
+    }
+}
